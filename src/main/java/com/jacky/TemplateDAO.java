56356151bc1adc9a156adbd9df72875391b07eb4
Harshit Kapoor <YATRAONLINE\v-harshit.kapoor@lap0001412.yatraonline.local>
2019-06-27 11:38:48 +0530

    Third

---
+++ src/main/java/com/jacky/TemplateDAO.java
@@ -0,0 +1,18 @@
+package com.jacky;
+
+import io.dropwizard.hibernate.AbstractDAO;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class TemplateDAO extends AbstractDAO<Template> {
+
+    public TemplateDAO(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    public List<Template> findAll(){
+        return list(namedQuery("com.jacky.Template.findAll"));
+    }
+    
+}
